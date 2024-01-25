06bbf605a289eac30b03b7046da8e468af726dcc
Juan Hamilton <hedwards507@gmail.com>
2024-01-24 22:13:32 -0500

    Create models and apply initial migrations

--- kanban-backend/kanbanproject/tasks/models.py
+++ kanban-backend/kanbanproject/tasks/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+class Column(models.Model):
+    name = models.CharField(max_length=50)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    column = models.ForeignKey(Column, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+class Subtask(models.Model):
+    title = models.CharField(max_length=255)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
